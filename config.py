68ce319380f088294e584009cc3d102e7fd1da49
Farook13 <umarulfarook787@gmail.com>
2025-02-22 00:03:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,4 +25,4 @@ LOG_CHANNEL = environ.get("-1002467149516")
 # Bot Owner ID
 OWNER_ID = int(environ.get("OWNER_ID", "5032034594"))
 # Auto-delete time in seconds (e.g., 60 seconds)
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "30"))
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "30"))
