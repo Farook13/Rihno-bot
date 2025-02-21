cc6a7a28c394cfe3e25d9b8fc9bda54494b8c900
Farook13 <umarulfarook787@gmail.com>
2025-02-22 03:00:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:  # If using a class, ensure proper indentation
 
 # If not using a class, ensure no stray block starters
 BOT_TOKEN = environ.get('7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
-API_ID = int(environ.get('API_ID', '12618934
+API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('49aacd0bc2f8924add29fb02e20c8a16')
 DATABASE_URI = environ.get('mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'Cluster0')
