286cdb0cb2ee04e4e26de17b0d54c70a189283ef
Farook13 <umarulfarook787@gmail.com>
2025-02-21 21:15:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,24 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 # Telegram Bot Token from @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Telegram API ID and Hash from my.telegram.org
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("12618934"))
+API_HASH = os.getenv("49aacd0bc2f8924add29fb02e20c8a16")
 
+BOT_TOKEN = environ.get('BOT_TOKEN', '7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
 # MongoDB Configuration
-DATABASE_URI = os.getenv("DATABASE_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "AutoFilterBotDB")
+DATABASE_URI = environ.get("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Farook")
 
 # Force Subscribe Channel ID (e.g., -100123456789)
-AUTH_CHANNEL = os.getenv("AUTH_CHANNEL")
+AUTH_CHANNEL = environ.get("-1002256041072")
 
 # Log Channel ID for bot activities
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+LOG_CHANNEL = environ.get("-1002467149516")
 
 # Bot Owner ID
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
-
+OWNER_ID = int(environ.get("OWNER_ID", "5032034594"))
 # Auto-delete time in seconds (e.g., 60 seconds)
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "60"))
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "30"))
