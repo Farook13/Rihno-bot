08066bcf4a8534d0ba5e51a3100d7cc37c9c48d3
Farook13 <umarulfarook787@gmail.com>
2025-02-22 01:48:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,13 @@
 import re
-import os
 from os import environ,getenv
-from dotenv import load_dotenv
-
-load_dotenv()  # Load environment variables from .env file
-
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
-#---------------------------------------------------------------
-#---------------------------------------------------------------         ,
-
+from Script import script
 
 # Telegram Bot Token from @BotFather
-class Config:
-# Telegram API ID and Hash from my.telegram.orgAPI_ID = int(environ.get('API_ID', '12618934'))
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '12618934'))
-API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
+BOT_TOKEN = environ.get('BOT_TOKEN',"7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E")
 
+# Telegram API ID and Hash from my.telegram.org
+API_ID = int(environ.get('API_ID', '17264725'))
+API_HASH = environ.get('API_HASH', 'e7c6c1e727962d2ade50bald7f4fac8a')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
 # MongoDB Configuration
 DATABASE_URI = environ.get("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
@@ -37,5 +21,3 @@ LOG_CHANNEL = environ.get("-1002467149516")
 
 # Bot Owner ID
 OWNER_ID = int(environ.get("OWNER_ID", "5032034594"))
-# Auto-delete time in seconds (e.g., 60 seconds)
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "30"))
