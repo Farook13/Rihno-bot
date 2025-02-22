2e740fbeab0f46ab28bc3201bdcb3362908cb673
Farook13 <umarulfarook787@gmail.com>
2025-02-22 18:00:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from pyrogram.errors import UserNotParticipant
+from aiocache import cached
 from config import Config
 
+@cached(ttl=300)  # Cache for 5 minutes
 async def check_force_sub(client, user_id):
     try:
         await client.get_chat_member(Config.FORCE_SUB_CHANNEL, user_id)
