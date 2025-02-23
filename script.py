a608ba3ad55a990c68c95bf70d370c0e0f498333
Farook13 <umarulfarook787@gmail.com>
2025-02-24 01:13:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
 # Static templates for the bot
 FILE_CAPTION = "<b>{file_caption}</b>\nSize: {file_size}\nUploaded by {SUPPORT_CHAT}"
 IMDB_TEMPLATE_TXT = "<b>Query:</b> {query}\n\n🏷 <b>Title:</b> <a href={url}>{title}</a>\n🎭 <b>Genres:</b> {genres}\n📆 <b>Year:</b> {year}\n🌟 <b>Rating:</b> {rating}/10\n\nSupport: {SUPPORT_CHAT}"
-​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
