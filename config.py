28f0fc2d56bbca874de5a4d548cf02c47998c772
Farook13 <umarulfarook787@gmail.com>
2025-02-23 09:25:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ  # Assuming environ is used as in the error
 
 # Configuration class or direct variables
 class Config:  # If using a class, ensure proper indentation
-    BOT_TOKEN = environ.get('BOT_TOKEN','7860780283:AAFt9MlBsWL7_qVZi87cQL01WbEaqOB8mV8')
+    BOT_TOKEN = environ.get('BOT_TOKEN','7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
     API_ID = int(environ.get('API_ID', '12618934'))
     API_HASH = environ.get('API_HASH','49aacd0bc2f8924add29fb02e20c8a16')
     MONGO_URI = environ.get('MONGO_URI','mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -14,7 +14,7 @@ class Config:  # If using a class, ensure proper indentation
     AUTO_DELETE_TIME = int(environ.get('AUTO_DELETE_TIME', '60'))
 
 # If not using a class, ensure no stray block starters
-BOT_TOKEN = environ.get('BOT_TOKEN','7860780283:AAFt9MlBsWL7_qVZi87cQL01WbEaqOB8mV8')
+BOT_TOKEN = environ.get('BOT_TOKEN','7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH','49aacd0bc2f8924add29fb02e20c8a16')
 MONGO_URI = environ.get('MONGO_URI','mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
