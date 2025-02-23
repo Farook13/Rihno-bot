cce6f75abab5bbedd7122281f404809b30c17c14
Farook13 <umarulfarook787@gmail.com>
2025-02-24 01:38:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 from config import LOGGER
 
 def get_file_size(file):
-    size = file.file_size
-    for unit in ['B', 'KB', 'MB', 'GB']:
-        if size < 1024:
-            return f"{size:.2f} {unit}"
-        size /= 1024
-    return f"{size:.2f} TB"
+ size = file.file_size
+ for unit in ['B', 'KB', 'MB', 'GB']:
+ if size < 1024:
+ return f"{size:.2f} {unit}"
+ size /= 1024
+ return f"{size:.2f} TB"
