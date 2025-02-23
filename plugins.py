d0ac1efd386fadc1e34cd4e777fcdc34af09a884
Farook13 <umarulfarook787@gmail.com>
2025-02-24 02:20:40 +0530

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -1,8 +1,13 @@
 from aiohttp import web
 
 async def web_server():
- async def hello(request):
- return web.Response(text="Telegram Movie Bot is running!")
- app = web.Application()
- app.router.add_get('/', hello)
- return app
+    async def hello(request):
+        return web.Response(text="Telegram Movie Bot is running!")
+    
+    app = web.Application()
+    app.router.add_get('/', hello)
+    return app
+
+# To actually run the server, you would typically add:
+# if __name__ == '__main__':
+#     web.run_app(web_server())
